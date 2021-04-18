f0dd050ee8f1bc376a0c7cf1f7f5a4ec8c9396ab
Shaikh Abu Faisal <60063407+sk7050@users.noreply.github.com>
2021-04-18 16:09:33 +0600

    Add files via upload

---
+++ user_account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views 
+
+urlpatterns = [
+    path('',views.login_user, name='login'),
+    path('signup/',views.signup_user, name='signup'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    ]
